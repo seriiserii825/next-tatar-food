e2770ccf528acba03720fa695f2276a693be48b7
serii burduja <seriiburduja@gmail.com>
2026-03-03 13:12:30 +0200

    feat: menu set active link

--- src/app/components/UI/Menu.tsx
+++ src/app/components/UI/Menu.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 export default function Menu() {
@@ -11,10 +14,21 @@ export default function Menu() {
     { name: "About", href: "/about" },
   ];
 
+  function setActiveLink(href: string): string {
+    const pathname = usePathname();
+    if (pathname === href) {
+      return "text-amber-600";
+    }
+    return "";
+  }
+
   return (
     <nav className="hidden md:flex gap-8 text-sm font-medium text-zinc-600">
       {links.map((link, index) => (
-        <Link href={link.href} key={index} className="hover:text-amber-600 transition">
+        <Link
+          href={link.href}
+          key={index}
+          className={`hover:text-amber-600 transition duration-300 ${setActiveLink(link.href)}`}>
           {link.name}
         </Link>
       ))}
