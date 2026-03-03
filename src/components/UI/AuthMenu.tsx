b857cbfeef0e0027dcc190d3fe649d1bc3c81570
seriiserii825 <seriiburduja@gmail.com>
2026-03-03 21:37:00 +0200

    feat: auth icon user

--- src/components/UI/AuthMenu.tsx
+++ src/components/UI/AuthMenu.tsx
@@ -1,10 +1,10 @@
 "use client";
-
-import { authClient } from "@/lib/auth-client";
 import { siteConfig } from "@/config/siteConfig";
 import useSetActiveLink from "@/hooks/useSetActiveLink";
-import { useRouter } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
+import { User } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function AuthMenu() {
   const { data: session, isPending } = authClient.useSession();
@@ -20,10 +20,13 @@ export default function AuthMenu() {
   if (session) {
     return (
       <nav className="flex items-center gap-4">
-        <span className="text-sm text-gray-500">{session.user.email}</span>
+        <span className="flex items-center gap-2 border-b border-gray-400 text-sm text-gray-500">
+          <User className="text-muted-foreground h-5 w-5" />
+          {session.user.name}
+        </span>
         <button
           onClick={handleLogout}
-          className="transition duration-300 hover:text-amber-600"
+          className="transition duration-300 text-sm hover:text-amber-600"
         >
           Logout
         </button>
