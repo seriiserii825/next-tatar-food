3242c5d4ef745db545c9708894688796891a3f19
serii burduja <seriiburduja@gmail.com>
2026-03-03 16:07:55 +0200

    feat: login form validation

--- src/forms/LoginForm.tsx
+++ src/forms/LoginForm.tsx
@@ -4,62 +4,84 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { Input } from "@/components/ui/input";
 import ShowError from "@/components/UI/ShowError";
+import { z } from "zod";
 
 export function LoginForm() {
-  const [firstTouch, setFirstTouch] = useState(false);
-  const [form, setForm] = useState({
+  const schema = z
+    .object({
+      email: z.string().email("Invalid email"),
+      password: z.string().min(6, "Min 6 characters"),
+      confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords do not match",
+      path: ["confirmPassword"],
+    });
+
+  type FormData = z.infer<typeof schema>;
+
+  const [form, setForm] = useState<FormData>({
     email: "",
     password: "",
     confirmPassword: "",
   });
-  const passwordsMatch = form.password === form.confirmPassword;
+  const [firstTouch, setFirstTouch] = useState(false);
+  const [errors, setErrors] = useState<{
+    email?: string[];
+    password?: string[];
+    confirmPassword?: string[];
+  }>({});
 
   function onSubmit(e: React.FormEvent) {
     e.preventDefault();
     setFirstTouch(true);
-    if (!form.email || !form.password || !form.confirmPassword) {
-      toast.error("Please fill in all fields!");
+
+    const result = schema.safeParse(form);
+    if (!result.success) {
+      const errors = result.error.flatten().fieldErrors;
+      setErrors(errors);
       return;
     }
-    if (!passwordsMatch) {
-      toast.error("Passwords do not match!");
-    }
+    setErrors({});
+    console.log(result.data, "result.data");
+    toast.success("Form submitted successfully!");
+    // result.data — типизированные данные
   }
 
   return (
-    <form onSubmit={onSubmit} className="max-w-lg mx-auto flex flex-col gap-4">
+    <form onSubmit={onSubmit} autoComplete="off" className="max-w-lg mx-auto flex flex-col gap-4">
       <Input
         type="email"
         name="my-email"
-        autoComplete="off"
+        autoComplete="nope"
         placeholder="Email"
-        aria-invalid={!form.email && firstTouch}
+        aria-invalid={errors.email && firstTouch}
         value={form.email}
         onChange={(e) => setForm({ ...form, email: e.target.value })}
         className="border p-2 rounded"
       />
-      {firstTouch && !form.email && <ShowError error="Email is required!" />}
+      {firstTouch && errors.email && <ShowError error={errors.email[0]} />}
       <Input
         type="password"
         name="my-password"
         placeholder="Password"
-        autoComplete="off"
-        aria-invalid={!form.password && firstTouch}
+        autoComplete="new-password"
+        aria-invalid={errors.password && firstTouch}
         value={form.password}
         onChange={(e) => setForm({ ...form, password: e.target.value })}
         className="border p-2 rounded"
       />
-      {firstTouch && !form.password && <ShowError error="Password is required!" />}
+      {firstTouch && errors.password && <ShowError error={errors.password[0]} />}
       <Input
         type="password"
         name="my-confirm-password"
         placeholder="Confirm Password"
         value={form.confirmPassword}
         onChange={(e) => setForm({ ...form, confirmPassword: e.target.value })}
-        aria-invalid={!passwordsMatch && firstTouch}
+        aria-invalid={errors.confirmPassword && firstTouch}
         className="border p-2 rounded"
       />
-      {firstTouch && !passwordsMatch && <ShowError error="Passwords do not match!" />}
+      {firstTouch && errors.confirmPassword && <ShowError error={errors.confirmPassword[0]} />}
 
       <button type="submit" className=" bg-blue-500 text-white p-2 rounded">
         Register
