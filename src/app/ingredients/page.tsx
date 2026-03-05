0e2860af9b0d571586adf4891298771326f470ed
seriiserii825 <seriiburduja@gmail.com>
2026-03-05 21:32:02 +0200

    feat: ingredients check auth to show edit fields

--- src/app/ingredients/page.tsx
+++ src/app/ingredients/page.tsx
@@ -2,22 +2,28 @@ import IngredientsTable from "@/components/UI/IngredientsTable";
 import PageContent from "@/components/UI/PageContent";
 import Title from "@/components/UI/Title";
 import IngredientForm from "@/forms/IngredientForm";
+import { requireSession } from "@/lib/auth-session";
 
-export default function Page() {
+export default async function Page() {
+  const { session, error } = await requireSession();
   return (
     <>
       <div className="pb-24">
         <Title />
         <PageContent />
-        <div className="mx-auto max-w-xl">
-          <h3 className="mb-4 text-center text-2xl font-semibold">
-            Add New Ingredient
-          </h3>
-          <IngredientForm />
-        </div>
-        <div className="mx-auto mt-16 max-w-4xl">
-          <IngredientsTable />
-        </div>
+        {session && (
+          <>
+            <div className="mx-auto max-w-xl">
+              <h3 className="mb-4 text-center text-2xl font-semibold">
+                Add New Ingredient
+              </h3>
+              <IngredientForm />
+            </div>
+            <div className="mx-auto mt-16 max-w-4xl">
+              <IngredientsTable />
+            </div>
+          </>
+        )}
       </div>
     </>
   );
