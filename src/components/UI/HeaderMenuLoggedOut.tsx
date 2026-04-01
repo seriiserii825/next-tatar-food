c53c320eec90912b8320db919f828a885459f251
serii burduja <seriiburduja@gmail.com>
2026-04-01 17:30:52 +0300

    feat: header menu logged out active

--- src/components/UI/HeaderMenuLoggedOut.tsx
+++ src/components/UI/HeaderMenuLoggedOut.tsx
@@ -1,12 +1,16 @@
+import { usePathname } from "next/navigation";
 import Button from "./Button";
 
 export default function HeaderMenuLoggedOut() {
+  const pathName = usePathname();
+  const loginActive = pathName === "/login" ? "primary" : "secondary";
+  const registerActive = pathName === "/register" ? "primary" : "secondary";
   return (
     <div className="flex items-center gap-5">
-      <Button href="/login" as="link" variant="primary" size="sm">
+      <Button href="/login" as="link" variant={loginActive} size="sm">
         Login
       </Button>
-      <Button href="/register" as="link" variant="secondary" size="sm">
+      <Button href="/register" as="link" variant={registerActive} size="sm">
         Register
       </Button>
     </div>
