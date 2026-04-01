9369b70f99e438adb00053d0f28041ccb0431ddb
serii burduja <seriiburduja@gmail.com>
2026-04-01 16:05:57 +0300

    feat: auth register form

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -4,11 +4,16 @@ import { TRegisterForm } from "@/types/TRegisterForm";
 import { prisma } from "@/utils/prisma";
 
 export async function registerUser(formData: TRegisterForm) {
-  const user = await prisma.user.create({
-    data: {
-      email: formData.email,
-      password: formData.password,
-    },
-  });
-  return user;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: formData.email,
+        password: formData.password,
+      },
+    });
+    return user;
+  } catch (error) {
+    console.error("Error registering user:", error);
+    throw new Error("Failed to register user");
+  }
 }
