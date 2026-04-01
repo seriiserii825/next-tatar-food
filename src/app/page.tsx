fe42cc390ec8f737313238d19801e00647f9b866
serii burduja <seriiburduja@gmail.com>
2026-04-01 12:35:06 +0300

    feat: first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">Home</h1>
+    </div>
+  );
+}
